0e70b59d461e11449edf57abf2ac460ad6d9f4f4
CinQue Wilson <parallax707@gmail.com>
2022-01-31 17:45:25 -0500

    project commit 1

---
+++ src/test/java/com/example/gamedev/GamedevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamedevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
