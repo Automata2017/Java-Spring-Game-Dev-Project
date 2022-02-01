aaf2611ad011297958d157b3bf68d2b4ee546075
CinQue Wilson <parallax707@gmail.com>
2022-02-01 00:11:02 -0500

    project commit 5

---
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/Controller/CategoryController.java
@@ -0,0 +1,19 @@
+package com.gamedev.gamedevproper.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+public class CategoryController {
+
+
+
+    @GetMapping(path = "/hello-world/")
+    public String getHelloWorld(){
+        return "Hello World";
+    }
+
+
+}
