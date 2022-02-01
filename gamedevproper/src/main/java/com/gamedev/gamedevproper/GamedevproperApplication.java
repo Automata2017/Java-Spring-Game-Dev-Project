efc14b8fced45c41d309d82f599b371f19f80b4a
CinQue Wilson <parallax707@gmail.com>
2022-01-31 23:27:28 -0500

    project commit 2

---
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/GamedevproperApplication.java
@@ -0,0 +1,13 @@
+package com.gamedev.gamedevproper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedevproperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamedevproperApplication.class, args);
+    }
+
+}
---
+++ gamedevproper/src/test/java/com/gamedev/gamedevproper/GamedevproperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamedev.gamedevproper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamedevproperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
