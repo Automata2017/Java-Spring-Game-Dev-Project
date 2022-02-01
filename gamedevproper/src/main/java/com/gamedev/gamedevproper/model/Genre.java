5212a869eb763e4d51b7596e69f4f9caa8add629
CinQue Wilson <parallax707@gmail.com>
2022-02-01 14:22:56 -0500

    project commit 18

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/model/Genre.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/model/Genre.java
@@ -1,5 +1,10 @@
 package com.gamedev.gamedevproper.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "genres")
 public class Genre {
 
     private Long id;
