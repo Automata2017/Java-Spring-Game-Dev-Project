bfbb6590ce560ed10d77109a6726e1b22fbc4b62
CinQue Wilson <parallax707@gmail.com>
2022-02-01 11:58:35 -0500

    project commit 16

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/model/Genre.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/model/Genre.java
@@ -6,6 +6,9 @@ public class Genre {
     private String name;
     private String description;
 
+    public Genre() {
+    }
+
     public Genre(Long id, String name, String description) {
         this.id = id;
         this.name = name;
