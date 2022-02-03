595b354c968ac1c33f41106da776f2524e7dfa45
CinQue Wilson <parallax707@gmail.com>
2022-02-02 23:52:14 -0500

    project commit 73

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/security/SecurityConfigurer.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/security/SecurityConfigurer.java
@@ -3,6 +3,7 @@ package com.gamedev.gamedevproper.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -37,5 +38,12 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Bean public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+
+
 
 }
