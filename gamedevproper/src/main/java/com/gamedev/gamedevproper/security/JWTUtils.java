cc56c671bb3ba60dee152b90d85aa68dba785b97
CinQue Wilson <parallax707@gmail.com>
2022-02-02 22:41:24 -0500

    project commit 63

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/security/JWTUtils.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/security/JWTUtils.java
@@ -1,22 +1,22 @@
 package com.gamedev.gamedevproper.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@Component
+@Service
 public class JWTUtils {
 
-    
+    String SECRET_KEY = "DevKey";
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+
 
 
 
