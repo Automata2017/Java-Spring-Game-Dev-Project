8b5023393de2e848faf7d7deaf51b95c8d0c63ae
CinQue Wilson <parallax707@gmail.com>
2022-02-02 22:42:09 -0500

    project commit 64

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/security/JWTUtils.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/security/JWTUtils.java
@@ -1,8 +1,11 @@
 package com.gamedev.gamedevproper.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +17,12 @@ public class JWTUtils {
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jwts.builder().setClaims(claims).setSubject(username).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
 
