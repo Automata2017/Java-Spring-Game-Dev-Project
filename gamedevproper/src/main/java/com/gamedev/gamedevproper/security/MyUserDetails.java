ae70654155b3992fb003894871a01cc4d8e18e5d
CinQue Wilson <parallax707@gmail.com>
2022-02-02 23:31:15 -0500

    project commit 67

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/security/MyUserDetails.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/security/MyUserDetails.java
@@ -1,4 +1,16 @@
 package com.gamedev.gamedevproper.security;
 
+import com.gamedev.gamedevproper.model.User;
+
 public class MyUserDetails {
+
+    private User user;
+    private String userName;
+    private String password;
+    private String emailAddress;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
 }
