269956000d05a3cf233d088b0a94dc37defb7cad
CinQue Wilson <parallax707@gmail.com>
2022-02-02 23:47:55 -0500

    project commit 71

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/security/SecurityConfigurer.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/security/SecurityConfigurer.java
@@ -1,12 +1,34 @@
 package com.gamedev.gamedevproper.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     private MyUserDetailsService myUserDetailsService;
 
     @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService){
+        this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers(
+                "/auth/users/", "/auth/users/login/", "/auth/users/register/", "/api/hello-world/").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
 }
