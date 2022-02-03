5019a2c8e55ebc4c40252ac29d4a6a3c51dab35f
CinQue Wilson <parallax707@gmail.com>
2022-02-02 22:36:46 -0500

    project commit 60

--- gamedevproper/src/main/java/com/gamedev/gamedevproper/security/JWTUtils.java
+++ gamedevproper/src/main/java/com/gamedev/gamedevproper/security/JWTUtils.java
@@ -3,6 +3,12 @@ package com.gamedev.gamedevproper.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Component
 public class JWTUtils {
 
@@ -12,6 +18,19 @@ public class JWTUtils {
     @Autowired
     private JWTUtils jwtUtils;
 
-    
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String AuthorizationHeader = httpServletRequest.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+        if (AuthorizationHeader != null && AuthorizationHeader.startsWith("Bearer ")) {
+            jwt = AuthorizationHeader.substring(7);
+            username = jwtUtils.extractUsername(jwt);
+        }
+
+
 
 }
